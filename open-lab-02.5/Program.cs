c696ae123ea5896769c54d2a56250da794ebae31
martinspilak <martinecko24@gmail.com>
2019-10-24 08:58:48 +0200

    open-lab-02.5

---
+++ open-lab-02.5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace open_lab_02._5
+{
+    class Program
+    {
+
+
+        public static bool DivisibleByFive(int a)
+        {
+            return true;
+        }
+    
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 5 == 0)
+            { 
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+        }
+    }
+}
+
